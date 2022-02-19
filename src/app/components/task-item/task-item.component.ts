9cd5fdf66ac9101b9f6454451da74e7927285667
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2022-02-19 21:31:23 +0100

    first commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { TaskService } from '../../services/task.service';
+
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = TASKS[0];
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+  faTimes = faTimes;
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task";
+
+export const TASKS:Task[] = [
+    {
+        id: 1,
+        text: "hacer la cama",
+        day: "5 de agosto a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "hacer la comida",
+        day: "5 de agosto a las 12:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "hacer la tarea",
+        day: "5 de agosto a las 12:00",
+        reminder: false
+    },
+]
